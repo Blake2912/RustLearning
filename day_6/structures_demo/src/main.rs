89301e2d61a143142040995ee5ebc508b82fe45d
Blake2912 <kcvarun29122001@gmail.com>
2024-01-13 15:11:56 +0530

    Struct completed

--- day_6/structures_demo/src/main.rs
+++ day_6/structures_demo/src/main.rs
@@ -1,14 +1,62 @@
-struct Rectangle(i32, i32);
-
-fn main() {
-    let rect1 = Rectangle(30,50);
-    let len = rect1.0;
-    let breadth = rect1.1;
-
-    let area = calculate_area(rect1);
-    println!("Area of the rectange length {len} and breadth {breadth} is {area}");
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    height: u32
 }
 
-fn calculate_area(rect: Rectangle) -> i32{
-    rect.0 * rect.1
+impl Rectangle{
+    fn perimeter(&self) -> u32{
+        2*(self.height + self.width)
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle{
+        width: 30,
+        height: 50
+    };
+
+    let rect2 = Rectangle{
+        width: 20,
+        height: 40
+    };
+
+    let sq = Rectangle::square(3);
+
+    dbg!(&sq);
+
+    println!("Rect = {:#?}", rect1);
+
+    dbg!(&rect1);
+
+    let area = calculate_area(&rect1);
+    println!("Area of a rectangle of Area: {}", area);
+    println!("The perimeter of the rectangle is: {}", rect1.perimeter());
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+    if rect1.can_hold(&rect2) {
+        println!("Rect 1 holds Rect2");
+    }
+    
+}
+
+fn calculate_area(rect: &Rectangle) -> u32{
+    rect.width * rect.height
 }
